b39cfc9ae784ea46c8840f5dd1ffc90712f551d3
slorinc <slorinc@gmail.com>
2017-06-11 20:50:30 +0200

    Add endpoints tests

--- src/test/java/com/tingco/codechallenge/elevator/controller/ElevatorEndpointTest.java
+++ src/test/java/com/tingco/codechallenge/elevator/controller/ElevatorEndpointTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -22,10 +23,26 @@ public class ElevatorEndpointTest {
     @Autowired
     private ElevatorEndpoint endpoint;
 
+    @Value("${com.tingco.elevator.numberofelevators}")
+    private int numberOfElevators;
+
     @Test
     public void ping() {
 
         Assert.assertEquals("pong", endpoint.ping());
+
+    }
+
+    @Test
+    public void requestElevator() {
+
+        Assert.assertEquals(0, endpoint.requestElevator(1).getId());
+
+    }
+    @Test
+    public void getElevators() {
+
+        Assert.assertEquals(numberOfElevators, endpoint.getElevators().size());
 
     }
 
